75d286734481dfcee6529e71d49579b7ae501d49
Ravi Poudel <ravi.poudel@nexdigitalstudio.com>
2025-07-13 07:54:54 +0545

    feat: add signup textediting controllers & validators

--- lib/features/auth/views/registration_page.dart
+++ lib/features/auth/views/registration_page.dart
@@ -1,9 +1,18 @@
 import 'package:awt_lab/core/widgets/buttons.dart';
 import 'package:flutter/material.dart';
 
-class RegistrationPage extends StatelessWidget {
+class RegistrationPage extends StatefulWidget {
   const RegistrationPage({super.key});
 
+  @override
+  State<RegistrationPage> createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,39 +25,57 @@ class RegistrationPage extends StatelessWidget {
             spacing: 20,
             children: [
               TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'First Name',
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Last Name',
-                ),
-              ),
-              TextFormField(
+                controller: _emailController,
+                validator: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    return null;
+                  } else {
+                    return 'Email is required';
+                  }
+                },
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Email',
                 ),
               ),
               TextFormField(
+                controller: _passwordController,
+                validator: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    return null;
+                  } else {
+                    return 'Password is required';
+                  }
+                },
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Password',
                 ),
               ),
               TextFormField(
+                controller: _confirmPasswordController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Confirm Password',
                 ),
+
+                validator: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    if (value != _passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  } else {
+                    return 'Confirm Password is required';
+                  }
+                },
               ),
 
               CustomButton(
                 title: 'Sign up',
                 onTap: () {
+
+                  
                   print('Sign up Tapped');
                 },
               ),
